979f079ca0266dc61d665323aec72b63e3676490
Shoab Ahmed <shoab10@gmail.com>
2023-02-15 17:29:53 -0800

    Add readme file and screenshots

---
+++ README.md
@@ -0,0 +1,11 @@
+# NYC Schools
+
+A sample app that lists schools in NYC area and display SAT stats for them
+
+This sample showcases:
+* Networking : Okhttp and Retrofit
+* DI : Hilt
+* UI : Android Views and MVVM
+* Testing : Added Tests for ViewModel
+
+<img src="screenshots/1.png"/> <img src="screenshots/2.png"/> <img src="screenshots/3.png"/>
--- app/src/main/java/com/shoab/nycschools/ui/MainActivity.kt
+++ app/src/main/java/com/shoab/nycschools/ui/MainActivity.kt
@@ -2,9 +2,13 @@ package com.shoab.nycschools.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.shoab.nycschools.R
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * A simple Activity that hosts the different fragments and navigation
+ */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
